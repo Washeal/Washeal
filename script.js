cba357990ec6239051254cf1b2c185dbb451f629
sooraj <soorajnambiar8@gmail.com>
2024-05-18 11:02:49 +0530

    ui

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const slider = document.getElementById('slider');
-const images = slider.querySelectorAll('img');
+const slider = document.getElementById("slider");
+const images = slider.querySelectorAll("img");
 let currentImage = 0;
 
 function changeImage() {
-  images[currentImage].classList.remove('active');  // Deactivate current image
+  images[currentImage].classList.remove("active"); // Deactivate current image
   currentImage = (currentImage + 1) % images.length; // Get next image index
-  images[currentImage].classList.add('active');    // Activate next image
+  images[currentImage].classList.add("active"); // Activate next image
   setTimeout(changeImage, 3000); // Call changeImage again after 3 seconds
 }
 
