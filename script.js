17ae26902a6fa0d473de6eac54c40753513bdb73
sooraj <soorajnambiar8@gmail.com>
2024-05-18 14:50:28 +0530

    added animations

--- script.js
+++ script.js
@@ -6,7 +6,22 @@ function changeImage() {
   images[currentImage].classList.remove("active"); // Deactivate current image
   currentImage = (currentImage + 1) % images.length; // Get next image index
   images[currentImage].classList.add("active"); // Activate next image
-  setTimeout(changeImage, 3000); 
+  setTimeout(changeImage, 3000);
 }
 
 changeImage();
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElement = document.querySelectorAll(".hidden");
+
+hiddenElement.forEach((el) => observer.observe(el));
